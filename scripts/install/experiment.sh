fefa01e6d2ecb734aa11fa058b38ecf7c03ca058
Daniel Topa <dantopa@gmail.com>
2026-09-06 18:03:06 -0600

    fixed experiment to capture experiment.sh

--- scripts/install/experiment.sh
+++ scripts/install/experiment.sh
@@ -8,14 +8,22 @@ start_time=${SECONDS}
 function new_step() {
     export counter=$((counter + 1))
     export subcounter=0
+    export subsubcounter=0
     echo ""
     echo "Step ${counter}: ${1}"
 }
 
 function sub_step() {
     export subcounter=$((subcounter + 1))
+    export subsubcounter=0
     echo ""
     echo "  Substep ${counter}.${subcounter}: ${1}"
+}
+
+function sub_substep() {
+    export subsubcounter=$((subsubcounter + 1))
+    echo ""
+    echo "  Substep ${counter}.${subcounter}.${subsubcounter}: ${1}"
 }
 
 function display_total_elapsed_time() {
@@ -26,41 +34,45 @@ function display_total_elapsed_time() {
     printf "Total elapsed time: %02d:%02d (MM:SS)\n" "$total_minutes" "$total_seconds"
 }
 
-new_step "Check that package list is passed in the argument"
+new_step "Housekeeping in advance of spack installs"
+
+sub_step "Check that package list is passed in the argument"
 if [[ $# -ne 1 ]]; then
     echo "Usage: $0 package-file"
     exit 1
 fi
 
 file="$1"
-new_step "file = ${file}"
+sub_step "package list passed: ${file}"
 
-new_step "Does file exist?"
+sub_step "Does file exist?"
 if [[ ! -f "${file}" ]]; then
     echo "ERROR: file not found: ${file}"
     exit 1
 fi
 
+sub_step "file located"
+
 repo="${pGithub}/spacklandia/builds/${HOSTNAME}"
-new_step "pointer to repo = ${repo}"
+sub_step "pointer to repo for build output = ${repo}"
 
-new_step "build directory structure"
-    sub_step "mkdir -p ${repo}/dependencies"
-              mkdir -p ${repo}/dependencies
-    sub_step "mkdir -p ${repo}/find"
-              mkdir -p ${repo}/find
-    sub_step "mkdir -p ${repo}/graph"
-              mkdir -p ${repo}/graph
-    sub_step "mkdir -p ${repo}/info"
-              mkdir -p ${repo}/info
-    sub_step "mkdir -p ${repo}/install"
-              mkdir -p ${repo}/install
-    sub_step "mkdir -p ${repo}/spec"
-              mkdir -p ${repo}/spec
-    sub_step "mkdir -p ${repo}/list-files"
-              mkdir -p ${repo}/list-files
+sub_step "build directory structure"
+    sub_substep "mkdir -p ${repo}/dependencies"
+                 mkdir -p ${repo}/dependencies
+    sub_substep "mkdir -p ${repo}/find"
+                 mkdir -p ${repo}/find
+    sub_substep "mkdir -p ${repo}/graph"
+                 mkdir -p ${repo}/graph
+    sub_substep "mkdir -p ${repo}/info"
+                 mkdir -p ${repo}/info
+    sub_substep "mkdir -p ${repo}/install"
+                 mkdir -p ${repo}/install
+    sub_substep "mkdir -p ${repo}/spec"
+                 mkdir -p ${repo}/spec
+    sub_substep "mkdir -p ${repo}/list-files"
+                 mkdir -p ${repo}/list-files
 
-new_step "begin loop over packages"
+sub_step "begin loop over packages"
 while IFS= read -r package; do
     [[ -z "${package}" || "${package}" == \#* ]] && continue
 
@@ -82,16 +94,16 @@ new_step "install package ${package}"
             spack spec "${package}" > "${repo}/spec/${package}.txt" 2>&1 &
 
   sub_step "spack install  ${package}"
-            spack install "${package}" > "${repo}/install/${package}.txt" 2>&1
+            # spack install "${package}" > "${repo}/install/${package}.txt" 2>&1
 
   sub_step "wait for all probe commands to finish writing"
 	    wait
 
-  sub_step "git add  ${repo}"
-            git add "${repo}"
+  sub_step "git add -A"
+            git add -A
 
-  sub_step "git commit -m  '${package}'"
-	    git commit -m  "${package}"
+  sub_step "git commit -m  '${package}' - '${HOSTNAME}'"
+	    git commit -m  "${package}  -  ${HOSTNAME}"
 
 done < "${file}"
 
@@ -101,10 +113,10 @@ done < "${file}"
 new_step "Add results to git repo"
     sub_step "store list file"
 	      cp "${file}" "${repo}/list-files/."
-    sub_step "git add  ${repo}"
-              git add "${repo}"
-    sub_step "git commit -m 'builds from ${file}'"
-              git commit -m "builds from ${file}"
+    sub_step "git add -A"
+              git add -A
+    sub_step "git commit -m 'builds from ${file} on ${HOSTNAME}'"
+              git commit -m "builds from ${file} on ${HOSTNAME}"
 
 printf "to push:\n cd $repo\n"
 
